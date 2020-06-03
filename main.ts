4bbebc6719a6872ab26be0e9bbce74e24182fd29
liuyeshu <798746621@qq.com>
2020-06-03 12:05:24 +0800

    add modify

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+microbit_CAR.setPixelRGB(microbit_CAR.Lights.Light1, RGBColors.Red)
+microbit_CAR.showLight()
 basic.forever(function () {
-	
+    if (microbit_CAR.LineSensorChk(microbit_CAR.LineSensorIndex.LEFT, microbit_CAR.LineState.WHITE) && microbit_CAR.LineSensorChk(microbit_CAR.LineSensorIndex.RIGHT, microbit_CAR.LineState.WHITE)) {
+        microbit_CAR.CarCtrl(microbit_CAR.CarState.FOREWARD)
+    } else {
+    	
+    }
 })
